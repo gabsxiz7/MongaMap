ef13fa5696501357e808ef7bdfb61b6b16776f0d
gabsxiz7 <gabriellalacerda10@gmail.com>
2025-01-23 01:50:19 -0300

    loading...

--- JS/home.js
+++ JS/home.js
@@ -1,11 +1,38 @@
-  // Adicione aqui scripts personalizados para manipulação dinâmica na página inicial
-  document.addEventListener('DOMContentLoaded', () => {
-    console.log('Página inicial carregada!');
-    // Exemplo: Adicionar um evento de clique no botão de cadastro
-    const cadastroBtn = document.querySelector('.btn-primary');
-    if (cadastroBtn) {
-        cadastroBtn.addEventListener('click', () => {
-            alert('Você será redirecionado para a página de cadastro!');
-        });
-    }
+/* fade in */
+const fadeElements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+    fadeElements.forEach(element => {
+        const position = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (position < windowHeight - 100) {
+            element.classList.add('show');
+        }
+    });
+});
+//inicializa o mapa
+var miniMap = L.map('mini-map').setView([-24.0911, -46.6206], 13);
+
+// adiciona a camada do OpenStreetMap
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(miniMap);
+
+//adiciona marcadores para pontos turísticos
+const pontosTuristicos = [
+    { coords: [-24.093180, -46.620378], nome: 'Etec Adolpho Berezin', descricao: 'Educação técnica de qualidade.' },
+];
+
+pontosTuristicos.forEach(ponto => {
+    L.marker(ponto.coords).addTo(miniMap)
+        .bindPopup(`<b>${ponto.nome}</b><br>${ponto.descricao}`);
+});
+
+/*navbar sanduba*/
+const menuToggle = document.getElementById("menuToggle");
+const navbarLinks = document.getElementById("navbarLinks");
+
+menuToggle.addEventListener("click", () => {
+  navbarLinks.classList.toggle("show");
 });
